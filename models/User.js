4e80d1fd2481dee8ba5e5f412a9d3bb29e1195f8
Tên_Của_Bạn <email_github_cua_ban@example.com>
2026-05-31 16:08:20 +0700

    4:08 31/5

--- models/User.js
+++ models/User.js
@@ -7,7 +7,8 @@ module.exports = (sequelize) => {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
     password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.ENUM('user', 'admin'), defaultValue: 'user' },
+    role: { type: DataTypes.STRING, defaultValue: 'user' },
+
     phone: { type: DataTypes.STRING, allowNull: true },
     address: { type: DataTypes.STRING, allowNull: true },
     city: { type: DataTypes.STRING, allowNull: true },
